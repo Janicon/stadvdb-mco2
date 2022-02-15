39682281157572f69e6e80b581be633ee8bbb2e1
Jan Canicon <Jan_Canicon@dlsu.edu.ph>
2022-02-16 01:54:41 +0800

    Reactivated connection of main page to db

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,7 @@ const testController = require('../controllers/testController.js');
 const app = express();
 
 app.get('/favicon.ico', mainController.getFavicon);
-app.get('/', mainController.getBlank);
+app.get('/', mainController.getIndex);
 app.get('/movie/:id', movieController.getMovie);
 app.post('/addMovie', movieController.addMovie);
 app.post('/movie/:id/edit', movieController.editMovie);
